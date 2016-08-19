aa04b85e9a876d050a5065389caa6c827a7f0c8f
tyang <435295045@qq.com>
2016-08-19 16:56:32 +0800

    增加控件

--- commdemo/src/main/java/com/yt/commdemo/mvp/presenter/fragment/WidgetFragment.java
+++ commdemo/src/main/java/com/yt/commdemo/mvp/presenter/fragment/WidgetFragment.java
@@ -1,21 +1,42 @@
 package com.yt.commdemo.mvp.presenter.fragment;
 
+import com.android.common.log.Logger;
 import com.android.common.mvp.presenter.FragmentPresenterImpl;
 import com.yt.commdemo.R;
 import com.yt.commdemo.mvp.view.WidgetFragmentView;
+import com.yt.commdemo.widget.SlideSwitch;
 
 import java.util.Arrays;
 import java.util.List;
+
+import butterknife.Bind;
 
 /**
  * Created by Administrator on 2016/8/6 0006.
  */
 public class WidgetFragment extends FragmentPresenterImpl<WidgetFragmentView>{
 
+    @Bind(R.id.swit)
+    SlideSwitch slide;
+
     @Override
     public void initData() {
         super.initData();
         List<String> strings = Arrays.asList(getResources().getStringArray(R.array.contact_option_array));
         mView.setDropdownListData(strings);
+
+        slide.setSlideListener(new SlideSwitch.SlideListener() {
+            @Override
+            public void open() {
+                Logger.e("slide", "open");
+            }
+
+            @Override
+            public void close() {
+                Logger.e("slide", "close");
+            }
+        });
     }
+
+
 }
